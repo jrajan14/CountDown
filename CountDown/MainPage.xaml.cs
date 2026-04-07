518607691ddf93270f61eecc41b9f4e42bf48fbd
jrajan14 <j.rajan14@yahoo.com>
2026-04-07 18:37:29 +0100

    Update MainPage.xaml.cs

--- CountDown/MainPage.xaml.cs
+++ CountDown/MainPage.xaml.cs
@@ -20,9 +20,6 @@ using Windows.UI.Xaml.Navigation;
 
 namespace CountDown
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         int selectedYear;
